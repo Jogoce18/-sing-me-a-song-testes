df14f949ae0164ca50bf6ed39b84cc7059a4e3bb
johana <gomezjohana655@gmail.com>
2022-10-03 11:42:49 -0300

    feat: Readme

--- back-end/tests/factories/recommendationFactory.ts
+++ back-end/tests/factories/recommendationFactory.ts
@@ -1,7 +1,7 @@
 import { faker } from "@faker-js/faker";
 
 export function recommendationFactory() {
-  const name = faker.lorem.words(2);
+  const name = faker.name.findName();
   const youtubeLink = `https://www.youtube.com/watch`;
 
   return { name, youtubeLink };
